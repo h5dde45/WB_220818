06806fe041b04176ed318ea7c36921b044cfe12a
tmvf <tmvf@yandex.ru>
2018-08-25 18:58:29 +0300

    03

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -4,17 +4,26 @@ import com.example.demo.domain.Message;
 import com.example.demo.domain.User;
 import com.example.demo.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping(value = "")
     public String greetingString() {
@@ -36,11 +45,28 @@ public class MainController {
     }
 
     @PostMapping(value = "main")
-    public String add(@AuthenticationPrincipal User user,
+    public String add(@RequestParam("file") MultipartFile file,
+            @AuthenticationPrincipal User user,
                       @RequestParam String text,
                       @RequestParam String tag,
-                      Model model) {
+                      Model model) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+
         messageRepo.save(message);
         model.addAttribute("messages", messageRepo.findAll());
 
--- src/main/resources/templates/greeting.ftl
+++ src/main/resources/templates/greeting.ftl
@@ -1,6 +1,5 @@
 <#import "parts/common.ftl" as c>
 
-    <@c.page>
-<div>|||||WB|||||</div>
-<a href="main">Main page</a>
+<@c.page>
+<h4>WB</h4>
 </@c.page>
